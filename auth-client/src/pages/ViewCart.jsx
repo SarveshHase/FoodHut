a44432e00c5fef5d3a3be17a86b0888cd1f7bfc8
SarveshHase <sarveshhase2020@gmail.com>
2024-04-24 02:51:27 +0530

    correction in itemsPrice calculation

--- auth-client/src/pages/ViewCart.jsx
+++ auth-client/src/pages/ViewCart.jsx
@@ -4,7 +4,7 @@ import CartFood from '../components/CartFood'
 
 function ViewCart() {
     const { cartItems, addToCart, removeItem } = useCartContext()
-    const itemsPrice = cartItems.reduce((a, c) => a + c.qty + c.price, 0)
+    const itemsPrice = cartItems.reduce((a, c) => a + (c.qty * c.price), 0)
     const taxPrice = itemsPrice * 0.14;
     const shippingPrice = itemsPrice > 2000 ? 0 : 20;
     const totalPrice = itemsPrice + shippingPrice + taxPrice;
